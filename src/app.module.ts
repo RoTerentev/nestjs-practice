4e29178561ea1368ae22c90f0605631a8c7c26ce
Roman Terentev <roman.dv.93@gmail.com>
2019-04-15 09:50:52 +1000

    ADD: db conf to  app module, validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HuntService } from './model/hunt/hunt.service';
+import { UserService } from './model/user/user.service';
+import { OfferModule } from './model/offer/offer.module';
+import { FowlModule } from './model/fowl/fowl.module';
+import { BookingModule } from './model/booking/booking.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // TODO: move to config file
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    OfferModule,
+    FowlModule,
+    BookingModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HuntService, UserService],
 })
 export class AppModule {}
