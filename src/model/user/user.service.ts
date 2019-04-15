9cc37d840203a1cb932aa3165ed04c7c5980cde8
Roman Terentev <roman.dv.93@gmail.com>
2019-04-15 16:07:51 +1000

    WIP: stub for users

--- src/model/user/user.service.ts
+++ src/model/user/user.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UserEntity, UserTypeEnum } from './user.entity';
+import { EntityManager } from 'typeorm';
+import { InjectEntityManager } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  // TODO: remove stub
+  private readonly users = [
+    this.entMngr.create(UserEntity, {
+      id: 1,
+      type: UserTypeEnum.ADMIN,
+      phone: "79993332200"
+    }),
+    this.entMngr.create(UserEntity, {
+      id: 2,
+      type: UserTypeEnum.GUIDE,
+      phone: "79993332210"
+    }),
+    this.entMngr.create(UserEntity, {
+      id: 4,
+      type: UserTypeEnum.GUIDE,
+      phone: "79993332211"
+    }),
+    this.entMngr.create(UserEntity, {
+      id: 3,
+      type: UserTypeEnum.HUNTER,
+      phone: "79993332220"
+    }),
+    this.entMngr.create(UserEntity, {
+      id: 5,
+      type: UserTypeEnum.HUNTER,
+      phone: "79993332221"
+    })
+  ]
+
+  constructor(
+    @InjectEntityManager()
+    private readonly entMngr: EntityManager
+  ){}
+
+  findOneByPhone(phone: string): Promise<UserEntity> {
+    // TODO: mwke real logic
+    const user = this.users.find(u => u.phone === phone);
+    if(!user) throw new BadRequestException();
+    return Promise.resolve(user);
+  }
+}
